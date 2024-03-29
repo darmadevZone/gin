a415a416c6a979a9dfc39f82d74260bdf29703bc
darmadevZone <darmadevdone@gmail.com>
2024-03-29 15:07:45 +0900

    crudの定義と実装

---
+++ mock/controllers/item_controller.go
@@ -0,0 +1,121 @@
+package controllers
+
+import (
+	"gin-market/mock/dto"
+	"gin-market/mock/services"
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
+
+type IItemController interface {
+	// 引数は、Pathに対してハンドリングをするために引数が`*gin.Context`となる
+	FindAll(ctx *gin.Context)
+	FindById(ctx *gin.Context)
+	Create(ctx *gin.Context)
+	Update(ctx *gin.Context)
+	Delete(ctx *gin.Context)
+}
+
+// Controller -> IService
+type ItemController struct {
+	services services.IItemService
+}
+
+// Delete implements IItemController.
+func (i *ItemController) Delete(ctx *gin.Context) {
+	itemId, err := strconv.ParseUint(ctx.Param("id"), 10, 64)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "Invalid id"})
+		return
+	}
+	err = i.services.Delete(uint(itemId))
+	if err != nil {
+		if err.Error() == "Item not found" {
+			ctx.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
+			return
+		}
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Unexpected Error"})
+		return
+	}
+	ctx.Status(http.StatusOK)
+}
+
+// Update implements IItemController.
+func (i *ItemController) Update(ctx *gin.Context) {
+	itemId, err := strconv.ParseUint(ctx.Param("id"), 10, 64)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "Invalid id"})
+		return
+	}
+	var input dto.UpdateItemInput
+	if err := ctx.ShouldBindJSON(&input); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	updateItem, err := i.services.Update(uint(itemId), input)
+	if err != nil {
+		if err.Error() == "Item not found" {
+			ctx.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
+			return
+		}
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Unexpected Error"})
+		return
+	}
+	ctx.JSON(http.StatusOK, gin.H{"data": updateItem})
+}
+
+// Create implements IItemController.
+func (i *ItemController) Create(ctx *gin.Context) {
+	// validation
+	var input dto.CreateItemInput
+	if err := ctx.ShouldBindJSON(&input); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	//
+	newItem, err := i.services.Create(input)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Unexpected Error"})
+		return
+	}
+	ctx.JSON(http.StatusCreated, gin.H{"data": newItem})
+
+}
+
+// FindById implements IItemController.
+func (i *ItemController) FindById(ctx *gin.Context) {
+	itemId, err := strconv.ParseUint(ctx.Param("id"), 10, 64)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "Invalid id"})
+		return
+	}
+	item, err := i.services.FindById(uint(itemId))
+	if err != nil {
+		if err.Error() == "Item not found" {
+			ctx.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
+			return
+		}
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Unexpected Error"})
+		return
+	}
+	ctx.JSON(http.StatusOK, gin.H{"data": item})
+
+}
+
+// FindAll implements IItemController.
+func (i *ItemController) FindAll(ctx *gin.Context) {
+	items, err := i.services.FindAll()
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{
+			"error": "Unexpected Error",
+		})
+		return
+	}
+	ctx.JSON(http.StatusOK, gin.H{"data": items})
+}
+
+func NewItemController(service services.IItemService) IItemController {
+	return &ItemController{services: service}
+}
